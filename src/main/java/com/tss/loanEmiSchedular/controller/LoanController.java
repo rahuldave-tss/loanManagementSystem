a750c74bd51cffd7d48fcf81fd13fc5cde8e44f0
Darshit Dhaduk <darshitdhaduk129@gmail.com>
2026-03-30 23:53:27 +0530

    fix some bugs and improme exceptions

--- src/main/java/com/tss/loanEmiSchedular/controller/LoanController.java
+++ src/main/java/com/tss/loanEmiSchedular/controller/LoanController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 
-@RequestMapping("/loans")
+@RequestMapping("/borrower/loans")
 @RequiredArgsConstructor
 public class LoanController {
 
